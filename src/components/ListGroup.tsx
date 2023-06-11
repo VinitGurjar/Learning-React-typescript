2f9372bb60c1eeadd5fa5ad05f0f8d5c6f3368d6
adhiramaa <vinitfagna@gmail.com>
2023-06-11 12:02:08 +0530

    Use Hook for state management

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,15 @@
-function ListGroup() {
-  var items = ["Naruto", "Sasuke", "Itachi", "Nagato", "Zoro"];
-  //in jsx we don't have a for loop
-  //using curly braces we can render data dynamically
+import { useState } from "react";
 
+function ListGroup() {
+  let items = ["Naruto", "Sasuke", "Itachi", "Nagato", "Zoro"];
+  //in jsx we don't have a for loop
+  let selectedIndex = 0;
+  //The event: MouseEvent below is a type annotation in typescript we can declare type of variable , parameter and so on.
+  //Managing state
+  //use state Hook  -A hook is function  Allows us to tab into buildin feature in react
+  useState;
+
+  //using curly braces we can render data dynamically
   return (
     <>
       <h1>List</h1>
@@ -11,9 +18,19 @@ function ListGroup() {
         {items.map((item, index) => (
           //Below Each Prop is on a single line after formating
           <li
-            className="list-group-item"
+            className={
+              //doing js for dynamic content
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={
+              //a Function we set selected inedx to the current item
+              () => {
+                selectedIndex = index;
+              }
+            }
           >
             {item}
           </li>
