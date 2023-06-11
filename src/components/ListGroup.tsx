98c401bab4d688d5b4cbb2e5ea16f1b3c2e8491d
adhiramaa <vinitfagna@gmail.com>
2023-06-11 10:50:09 +0530

    Implement conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
 function ListGroup() {
-  const items = ["Naruto", "Sasuke", "Itachi", "Nagato", "Zoro"];
+  var items = ["Naruto", "Sasuke", "Itachi", "Nagato", "Zoro"];
   //in jsx we don't have a for loop
   //using curly braces we can render data dynamically
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
